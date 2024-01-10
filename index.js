d209e885c0368df8f15c1b7f23294d7e13ecb2d0
saifulislamsuhanwd <137609106+saifulislamsuhanwd@users.noreply.github.com>
2024-01-10 20:18:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-{
-  "name" : "Saiful Islam"
-}
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, this is your Node.js server displaying text in the browser!');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
