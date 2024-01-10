9af4ebefcfba6068ee6e068fe6433b919413979f
saifulislamsuhanwd <137609106+saifulislamsuhanwd@users.noreply.github.com>
2024-01-10 20:08:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Define a simple GET endpoint
-app.get('/api/greeting', (req, res) => {
-  res.json({ message: 'Hello, this is your Express.js server!' });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+{
+  "name" : "Saiful Islam"
+}
